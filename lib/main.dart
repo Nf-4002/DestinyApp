c841ba55883b4b56b185995f94687d393c8cbdee
Nf-4002 <nargesfarahi15@gmail.com>
2024-10-22 19:08:31 +0430

    My Destiny project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+import 'story_brain.dart';
 
 void main() => runApp(Destiny());
 
@@ -13,7 +12,8 @@ class Destiny extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+
+StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
@@ -24,8 +24,15 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        // TODO: Step 1 - Add a linear gradient background to the container, using decoration property which takes a BoxDecoration. You can use Grey (shade 300), Pink (shade 900) and Deep Purple colors.
-        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.grey[300]!, Colors.pink[900]!, Colors.deepPurple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 15),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
           child: Column(
@@ -35,13 +42,13 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
-                    //TODO: Step 30.A - use textDirection to display the text in a Right to Left direction.
 
+                    storyBrain.getStory(),
+
+                    textDirection: TextDirection.rtl,
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 22.0,
+                      fontSize: 22,
                     ),
                   ),
                 ),
@@ -50,50 +57,62 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 child: TextButton(
                   onPressed: () {
-                    //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                    //TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons. (What code should you use to see changes live)
+
+                    setState(() {
+
+                      storyBrain.nextStory(1);
+                    });
                   },
                   style: TextButton.styleFrom(
-                    backgroundColor: Colors.purple[700],
-                    //TODO: Step 31.A use the shape property with RoundedRectangleBorder to give the text a rounded corner of 22 points.
+                    backgroundColor: Colors.purple[800],
+                    shape: RoundedRectangleBorder(
+
+                      borderRadius: BorderRadius.circular(22),
+                    ),
                   ),
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
-                    //TODO: Step 30.B - use textDirection to display the text in a Right to Left direction.
 
+                    storyBrain.getChoice1(),
+
+                    textDirection: TextDirection.rtl,
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 20.0,
+                      fontSize: 20,
                     ),
                   ),
                 ),
               ),
               SizedBox(
-                height: 30.0,
+                height: 20,
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this TextButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: TextButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.pink,
-                    //TODO: Step 31.B use the shape property with RoundedRectangleBorder to give the text a rounded corner of 22 points.
-                  ),
-                  child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
-                    //TODO: Step 30.B - use textDirection to display the text in a Right to Left direction.
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
 
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
+                      setState(() {
+
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.pink[900],
+                      shape: RoundedRectangleBorder(
+
+                        borderRadius: BorderRadius.circular(22),
+                      ),
+                    ),
+                    child: Text(
+
+                      storyBrain.getChoice2(),
+
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
                 ),
@@ -105,5 +124,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps.
